29d3647fdcbf354502d6133b562e80673f9a650c
davidpacecode <davidpace@gmail.com>
2026-05-09 09:22:14 -0500

    checking in the last bits including working random and about i think

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: %i[ home about ]
+  allow_unauthenticated_access only: %i[ home about random ]
 
   def home
   end
 
   def about
+  end
+
+  def random
   end
 end
