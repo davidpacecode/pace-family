24a365fa80b9430116ebacae31ed9eb2fd0ee107
davidpacecode <davidpace@gmail.com>
2026-05-09 23:34:35 -0500

    put in welcome message for authenticated users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_10_025912) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_034915) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_10_025912) do
     t.string "email_address", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "nickname"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
